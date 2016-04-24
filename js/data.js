1efe1fc10b517f64551b1aca75b9cd6f9e92cabc
Frank DelPidio <frankdaedalus@gmail.com>
2016-04-24 13:50:03 -0400

    pull data from data.js

--- js/data.js
+++ js/data.js
@@ -8,7 +8,8 @@ var fourSeasons = {
   image: "http://fourseasonsrestaurant.com/images/index/Four%20Seasons%20PDR1.jpg",
   phone: "212-754-9494",
   distance: "0.4 mile",
-  mapEmbed: "https://www.google.com/maps/embed/v1/directions?key=AIzaSyCBR22ALksTJlfUqgBbiblbe0koi2l6d44&origin=Trump+Tower&destination=Four+Seasons"
+  mapEmbed: "https://www.google.com/maps/embed/v1/directions?key=AIzaSyCBR22ALksTJlfUqgBbiblbe0koi2l6d44&origin=Trump+Tower&destination=Four+Seasons",
+  price: "$$$$"
  }
 
 var theModern = {
@@ -19,7 +20,8 @@ var theModern = {
   image: "http://nyjournal.squarespace.com/storage/modern.jpg",
   phone: "212-333-1220",
   distance: "0.3 mile",
-   mapEmbed: "https://www.google.com/maps/embed/v1/directions?key=AIzaSyCBR22ALksTJlfUqgBbiblbe0koi2l6d44&origin=Trump+Tower&destination=the+modern"
+   mapEmbed: "https://www.google.com/maps/embed/v1/directions?key=AIzaSyCBR22ALksTJlfUqgBbiblbe0koi2l6d44&origin=Trump+Tower&destination=the+modern",
+   price: "$$$"
 }
 
 var laGrenouille = {
@@ -30,7 +32,8 @@ var laGrenouille = {
   image: "https://i.ytimg.com/vi/Ah7h2mJwTOg/hqdefault.jpg",
   phone: "212-752-1495",
   distance: "0.2 mile",
-  mapEmbed: "https://www.google.com/maps/embed/v1/directions?key=AIzaSyCBR22ALksTJlfUqgBbiblbe0koi2l6d44&origin=Trump+Tower&destination=La+Grenouille"
+  mapEmbed: "https://www.google.com/maps/embed/v1/directions?key=AIzaSyCBR22ALksTJlfUqgBbiblbe0koi2l6d44&origin=Trump+Tower&destination=La+Grenouille",
+  price: "$$$$"
 }
 
 var leCirque = {
@@ -40,7 +43,8 @@ var leCirque = {
   address: "151 E 58th St, New York, NY 10022",
   image: "http://assets.nydailynews.com/polopoly_fs/1.368567!/img/httpImage/image.jpg",
   distance: "0.5 mile",
-  mapEmbed: "https://www.google.com/maps/embed/v1/directions?key=AIzaSyCBR22ALksTJlfUqgBbiblbe0koi2l6d44&origin=Trump+Tower&destination=le+cirque"
+  mapEmbed: "https://www.google.com/maps/embed/v1/directions?key=AIzaSyCBR22ALksTJlfUqgBbiblbe0koi2l6d44&origin=Trump+Tower&destination=le+cirque",
+  price: "$$$"
 }
 
 var felidia = {
@@ -50,10 +54,13 @@ var felidia = {
   address: "243 E 58th St, New York, NY 10022",
   image: "http://assets.nydailynews.com/polopoly_fs/1.368567!/img/httpImage/image.jpg",
   distance: "0.6 mile",
-  mapEmbed: "https://www.google.com/maps/embed/v1/directions?key=AIzaSyCBR22ALksTJlfUqgBbiblbe0koi2l6d44&origin=Trump+Tower&destination=felidia"
+  mapEmbed: "https://www.google.com/maps/embed/v1/directions?key=AIzaSyCBR22ALksTJlfUqgBbiblbe0koi2l6d44&origin=Trump+Tower&destination=felidia",
+  price: "$$$$$"
 }
 
 var locations = [felidia, leCirque, laGrenouille, theModern, fourSeasons]
+
+shuffle(locations)
 
 function shuffle(a) {
     var j, x, i;
@@ -65,27 +72,36 @@ function shuffle(a) {
     }
 }
 
-function createMap(location) {
-  var iframe = document.createElement('iframe')
-  // new_row.className = "aClassName";
-//   var html = '<iframe
-//   width="450"
-//   height="250"
-//   frameborder="0" style="border:0"
-//   src=' + location.mapEmbed + ' allowfullscreen>
-// </iframe>'
-  iframe.src = "iframe.src = 'data:text/html;charset=utf-8,'" + encodeURI(location.mapEmbed)
-  document.body.appendChild(iframe)
+$(document).ready(function(){
+  var biz = locations[0]
 
-}
+  $(".name").text("Name: " + biz.name)
+  $("#image").attr('src', biz.image)
+  $(".category").text("Category: " + biz.category)
+  $(".price").text("Price: " + biz.price )
+  $(".distance").text(biz.distance)
+})
+
+// function createMap(location) {
+//   var iframe = document.createElement('iframe')
+//   // new_row.className = "aClassName";
+// //   var html = '<iframe
+// //   width="450"
+// //   height="250"
+// //   frameborder="0" style="border:0"
+// //   src=' + location.mapEmbed + ' allowfullscreen>
+// // </iframe>'
+//   iframe.src = "iframe.src = 'data:text/html;charset=utf-8,'" + encodeURI(location.mapEmbed)
+//   document.body.appendChild(iframe)
+
+// }
 
 // function displayMap() {
 //   document.getElementById("myThing").addEventListener("click", createMap(locations[0]));
 // }
 
-document.getElementById("myThing").addEventListener("click", function(){ alert("Hello World!"); });
+//document.getElementById("myThing").addEventListener("click", function(){ alert("Hello World!"); });
 
-shuffle(locations)
 
 
 
