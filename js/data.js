92560c3bd87285e228a4a366fbcb5a14f942b789
Frank DelPidio <frankdaedalus@gmail.com>
2016-04-24 11:12:25 -0400

    rename to food to data

---
+++ js/data.js
@@ -0,0 +1,56 @@
+var fourSeasons = {
+  name: "Four Seasons",
+  rating: "4.6 Stars",
+  address: "E 52nd St",
+  category: "American",
+  image: "http://fourseasonsrestaurant.com/images/index/Four%20Seasons%20PDR1.jpg",
+  phone: "212-754-9494",
+  distance: "0.4 mile"
+
+ }
+var theModern = {
+  name: "theModern",
+  rating: "4.4 Stars",
+  category: "American",
+  address: "W 53 St.",
+  image: "http://nyjournal.squarespace.com/storage/modern.jpg",
+  phone: "212-333-1220",
+  distance: "0.3 mile"
+}
+
+var laGrenouille = {
+  name: "La Grenouille",
+  rating: "4.6 Stars",
+  category: "French",
+  address: "3 E 52nd St, New York, NY 10022",
+  image: "https://i.ytimg.com/vi/Ah7h2mJwTOg/hqdefault.jpg",
+  phone: "212-752-1495",
+  distance: "0.2 mile"
+}
+
+var leCirque = {
+  name: "Le Cirque",
+  rating: "4.2 Stars",
+  category: "French",
+  address: "151 E 58th St, New York, NY 10022",
+  image: "http://assets.nydailynews.com/polopoly_fs/1.368567!/img/httpImage/image.jpg",
+  distance: "0.5 mile"
+}
+
+var felidia = {
+  name: "Felidia",
+  rating: "4.1 Stars",
+  category: "Italian",
+  address: "243 E 58th St, New York, NY 10022",
+  image: "http://assets.nydailynews.com/polopoly_fs/1.368567!/img/httpImage/image.jpg",
+  distance: "0.6 mile"
+}
+
+// var 21 Club = { name: "21 Club" }
+// var Nobu Fifty Seven = { name: "Nobu Fifty Seven" }
+// var Jojo = { name: "Jojo" }
+// var Hillstone = { name: "Hillstone" }
+// var Betony = { name: "Betony" }
+// var Ma Peche = { name: "Ma Peche" }
+
+// }
